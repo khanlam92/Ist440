4c30b0ffc4a8d4883f26a334caeed4948be41ebd
Will <wcr5048@psu.edu>
2015-01-28 14:17:38 -0500

    fixed layout issues in index

--- php/create_user.php
+++ php/create_user.php
@@ -2,32 +2,16 @@
 <?php
 
 $servername = "localhost";
-$username = "dbuser";
-$password = "dbpassword";
-$dbname = "ist440w";
-
-$user_email = $_POST["email"];
-$user_password = $_POST["password"];
-$user_id = rand();
+$username = "root";
+$password = "";
+$dbname = "ist440test";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
 //Create the user
 if($conn)
 {
-	$m_query = "INSERT INTO Users (id, email, password) VALUES ('$user_id', '$user_email', '$user_password')";
-	if ($conn->query($m_query) === TRUE) {
-    	echo "success";
-    	//redirect success
-    	header("Location: http://localhost/IST440Project/index.php?resp=success"); 
-		exit();
-	} 
-	else {
-	    echo "error: " . $sql . "<br>" . $conn->error;
-	    //redirect success
-    	header("Location: http://localhost/IST440Project/index.php?resp=fail"); 
-		exit();
-	}
+	
 }
 else if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
